af175b7753336b6377f2a2eac2a31e4db3e6338a
breeze1298 <breezenadar12@gmail.com>
2021-01-15 11:48:28 +0530

    Done

---
+++ app/src/main/java/com/example/imeidemo/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.imeidemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView t1;
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        t1 = findViewById(R.id.textView);
+        b1 = findViewById(R.id.button);
+
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                @SuppressLint("MissingPermission") String imei = telephonyManager.getDeviceId();
+                t1.setText(imei);
+            }
+        });
+
+
+
+    }
+}
