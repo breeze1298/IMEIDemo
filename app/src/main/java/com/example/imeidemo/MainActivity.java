465d43685f35d568fdc685b6ef1f6131b09b39ee
Tushar patil <patiltushar9820@gmail.com>
2021-01-15 01:43:45 -0800

    firebase

--- app/src/main/java/com/example/imeidemo/MainActivity.java
+++ app/src/main/java/com/example/imeidemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.imeidemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -9,11 +10,22 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+
+    String TAG="mainactivity";
 
     TextView t1;
     Button b1;
@@ -21,11 +33,15 @@ public class MainActivity extends AppCompatActivity {
 
     //hi i am tushar
 
+
     // Hi i am Breeze From Nashik ok thanks
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //create firebase instance
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         t1 = findViewById(R.id.textView);
         b1 = findViewById(R.id.button);
@@ -37,6 +53,26 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 @SuppressLint("MissingPermission") String imei = telephonyManager.getDeviceId();
                 t1.setText(imei);
+                Map<String, Object> user = new HashMap<>();
+                user.put("first", "Ada");
+                user.put("last", "Lovelace");
+                user.put("born", 1815);
+
+// Add a new document with a generated ID
+                db.collection("users")
+                        .add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
             }
         });
 
